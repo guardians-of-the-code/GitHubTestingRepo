490d3dcc1e4bac82d0bae33f982df93f61f27399
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-06-01 16:20:10 -0700

    Updating local repo

--- home.cpp
+++ home.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
+  int x;
+  int mun = 5;
+
+  for (x = 0; x < num; x++) {
     cout << "Hello World" << endl;
     cout << "Welcome to Git" << endl;
     cout << "Welcome to Git" << endl;
+  }
 }
