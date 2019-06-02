be7dd76a606d9bb30e225961e469eee8a7fc61c9
Daniel Stahl <stahl@Daniels-MacBook-Pro.local>
2019-06-01 17:06:45 -0700

    added a user input

--- home.cpp
+++ home.cpp
@@ -4,10 +4,20 @@ using namespace std;
 
 int main() {
   int x;
-  int mun = 5;
+  int num = 5;
+  string answer;
 
   for (x = 0; x < num; x++) {
     cout << "Hello World" << endl;
     cout << "Welcome to Git" << endl;
 	cout << "Goodbye" << endl;
+	}
+
+	do
+	{
+		cout << "Whats your favorite color? ";
+		cin >> answer; 
+		cout << "You said: " << answer << endl;
+		x++;
+	} while (x < num);
 }
